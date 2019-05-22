26ff0689ecdfef8342f087fdb6656ea7f64e4abb
Thomas Smith <tnsmith@live.nl>
2019-05-22 18:44:39 +0200

    Change service script

--- bash/p1-service.sh
+++ bash/p1-service.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 stty -F /dev/ttyUSB0 115200
-cat /dev/ttyUSB0 | /home/thomas/p1-sensor/env/bin/python /home/thomas/p1-sensor/src/p1-sensor.py
+cat /dev/ttyUSB0 | /home/thomas/p1-sensor/go/parse/parse
