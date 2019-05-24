6d9ae0a5d8f0e17a720781c4594453ccd848df30
Thomas Smith <tnsmith@live.nl>
2019-05-24 15:09:41 +0200

    Include timestamp

--- go/parse/parse.go
+++ go/parse/parse.go
@@ -11,14 +11,17 @@ import (
 func main() {
 	scanner := bufio.NewReader(os.Stdin)
 
+	var timestamp string
 	for {
 		line, err := scanner.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
-		if strings.Contains(line, "1-0:1.7.0") {
+		if strings.Contains(line, "0-0:1.0.0") {
+			timestamp = strings.Split(line, "(")[1][:12]
+		} else if strings.Contains(line, "1-0:1.7.0") {
 			kwUsage := strings.Split(strings.Split(line, "(")[1], "*")[0]
-			fmt.Println(kwUsage)
+			fmt.Printf("%s, %s\n", timestamp, kwUsage)
 		}
 	}
 }
